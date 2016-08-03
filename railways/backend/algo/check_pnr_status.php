00dc44ad627c89c21ae1a0e61e04b9f01b64702e
sagar-cenation <sagarkeshri@rocketmail.com>
2016-08-03 23:41:47 +0530

    made new_seat_availability responsive wid a few bugs to be solved

--- railways/backend/algo/check_pnr_status.php
+++ railways/backend/algo/check_pnr_status.php
@@ -5,13 +5,13 @@
     ////$pnr = "2418566933";
 
 //    $apikey = "uucxi9379";//satenderjpr@gmail.com
-   // $apikey = "ttemb6830";//singhpalarashakti@gmail.com
+    $apikey = "ttemb6830";//singhpalarashakti@gmail.com
    // $apikey = "ootzm7275";//satendersvnit@gmail.com
     // $apikey = "eumbm2216";//singhrathoresatender@gmail.com
     // $apikey = "wqyoc1399"; //renurathorejpr@gmail.com
     // $apikey = "budyl6423";//yashagarwaljpr@gmail.com
-    $apikey = "zlzou2003";//satendersinghpalara@gmail.com
-    //$apikey = "iyihg4653";//jagdishsinghrjpr@gmail.com
+    // $apikey = "zlzou2003";//satendersinghpalara@gmail.com
+    // $apikey = "iyihg4653";//jagdishsinghrjpr@gmail.com
 
     // $apikey = "okogk2695";//theyashagarwal21@gmail.com
 
