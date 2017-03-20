f79059b572c3b7ac2f8336cd81201d912e2fa38f
sagar-cenation <sagarkeshri@rocketmail.com>
2017-03-20 15:38:39 +0530

    solved bugs

--- railways/frontend/pnr_status/summary.php
+++ railways/frontend/pnr_status/summary.php
@@ -13,11 +13,11 @@ $pdf->AddPage();
 $pdf->SetFont('Arial','B',16);
 $pdf->Cell(0,10,$pnr,0,0);
 $pdf->Cell(0,10,"Train Name : ",0,1);
-$pdf->Cell(0,10,$train_name,0,0);
+$pdf->Cell(0,10,$_SESSION['train_name'],0,0);
 $pdf->Cell(0,10,"Reservation upto : ",0,1);
-$pdf->Cell(0,10,$to_station,0,0);
+$pdf->Cell(0,10,$_SESSION['to_station'],0,0);
 $pdf->Cell(0,10,"Boarding Point : ",0,1);
-$pdf->Cell(0,10,$boarding_point,0,0);
+$pdf->Cell(0,10,$_SESSION['boarding_point'],0,0);
 $pdf->Output();
 
 ?>
