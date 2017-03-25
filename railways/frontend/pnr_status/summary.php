331ccaa760869715a60816875ea153594f9207d5
Satender Singh Rathore <satenderjpr@gmail.com>
2017-03-25 17:13:04 +0530

    add data to pdf

--- railways/frontend/pnr_status/summary.php
+++ railways/frontend/pnr_status/summary.php
@@ -1,6 +1,8 @@
+
 <?php
+// include("pdf.php");
 require('fpdf/fpdf.php');
-// include('index.php');
+$name = 'raja';
 session_start();
 $pnr = $_SESSION['pnr'];
 $train_num = $_SESSION['train_num'];
@@ -16,41 +18,27 @@ $from_station = $_SESSION['from_station'];
 $from_station_code = $_SESSION['from_station_code'];
 $class = $_SESSION['class'];
 $doj = $_SESSION['doj'];
-$reservation_upto = $_SESSION['reservation_upto'];
-
-// print_r($_SESSOIN['pnr']);
-// echo $_SESSION['pnr'];
-
-// print_r($train_num);
-$pdf=new FPDF();
-$pdf->AddPage();
-$pdf->SetFont('Arial','B',16);
-$pdf->Cell(0,10,$pnr,0,0);
-$pdf->Cell(0,10,"Train Name : ",0,1);
-$pdf->Cell(0,10,$_SESSION['train_name'],0,0);
-$pdf->Cell(0,10,"Reservation upto : ",0,1);
-$pdf->Cell(0,10,$_SESSION['to_station'],0,0);
-$pdf->Cell(0,10,"Boarding Point : ",0,1);
-$pdf->Cell(0,10,$_SESSION['boarding_point'],0,0);
-$pdf->Output();
-
-?>
-
-<?php
-// include("pdf.php");
-require('fpdf/fpdf.php');
-$name = 'raja';
-session_start();
-$pnr = $_SESSION['pnr'];
+// $reservation_upto = $_SESSION['reservation_upto'];
 
 
 
 $pdf=new FPDF();
 $pdf->AddPage();
 $pdf->SetFont('Arial','B',16);
-$pdf->Cell(0,10,"SARDAR VALLABHBHAI NATIONAL INSTITUTE OF TECHNOLOGY",1,1,'C');
-$pdf->Cell(0,10,"HOSTEL : TAGORE BHAVAN",1,1,'C');
+$pdf->Cell(0,10,"JOURNEY DETAILS",1,1,'C');
+$pdf->Cell(0,10,"HOSTEL : TAGORE BHAVAN",1,1);
 //$pdf->Cell(60,10,"Hello {$name}",1,1,'C');
+$pdf->Cell(100,10,"Pnr number : {$pnr}",1,1);
+$pdf->Cell(100,10,"Train number : {$train_num}",1,1);
+$pdf->Cell(100,10,"Train name : {$train_name}",1,1);
+$pdf->Cell(100,10,"From station : {$from_station}",1,1);
+$pdf->Cell(100,10,"To station : {$to_station_code}",1,1);
+$pdf->Cell(100,10,"Doj : {$doj}",1,1);
+$pdf->Cell(100,10,"Boarding point : {$boarding_point}",1,1);
+$pdf->Cell(100,10,"Total Passengers : {$total_passengers}",1,1);
+$pdf->Cell(100,10,"Class : {$class}",1,1);
+$pdf->Cell(100,10,"NAME : {$pnr}",1,1);
+$pdf->Cell(100,10,"NAME : {$pnr}",1,1);
 $pdf->Cell(100,10,"NAME : {$pnr}",1,1);
 $pdf->Cell(100,10,"ROOM NO : AG-25",1,1);
 $pdf->Cell(100,10,"ADMISSION NO:U14CO052",1,1);
